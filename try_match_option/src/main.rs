1d17ef0446b843759e01e5e15d1423d7da148c38
g7xcy <g7xcy@outlook.com>
2024-04-17 03:17:59 +0800

    Add match in bind Option

---
+++ try_match_option/src/main.rs
@@ -0,0 +1,23 @@
+fn plus_one(x: &Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(num) => Some(num + 1),
+    }
+}
+
+fn print_option_i32(x: &Option<i32>) {
+    match x {
+        None => println!("None"),
+        Some(num) => println!("{num}"),
+    }
+}
+
+fn main() {
+    let three = Some(3);
+    let four = plus_one(&three);
+    let none = plus_one(&None);
+
+    print_option_i32(&three);
+    print_option_i32(&four);
+    print_option_i32(&none);
+}
