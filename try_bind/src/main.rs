d1fc48044a877226bd54d98cba046098adf62c6a
g7xcy <g7xcy@outlook.com>
2024-04-17 02:36:36 +0800

    Use impl

---
+++ try_bind/src/main.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+impl Coin {
+    fn value_in_cents(&self) -> u8 {
+        match self {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
+        }
+    }
+}
+
+fn main() {
+    let coin: Coin = Coin::Quarter(UsState::Alaska);
+    println!("The value of coin is {}.", coin.value_in_cents());
+}
--- try_match/src/main.rs
+++ try_match/src/main.rs
@@ -5,6 +5,20 @@ enum Coin {
     Quarter,
 }
 
+impl Coin {
+    fn value_in_cents(&self) -> u8 {
+        match self {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            }
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+}
+
 fn main() {
     println!("match模式匹配后面的表达式不要求结果为bool类型。");
     println!("match匹配分支的表达式结果是match的返回值。");
@@ -13,26 +27,14 @@ fn main() {
         "{}"
     );
     let coin: Coin = Coin::Penny;
-    println!("Value of coin is {}", value_in_cents(&coin));
+    println!("Value of coin is {}", coin.value_in_cents());
 
     let coin: Coin = Coin::Nickel;
-    println!("Value of coin is {}", value_in_cents(&coin));
+    println!("Value of coin is {}", coin.value_in_cents());
 
     let coin: Coin = Coin::Dime;
-    println!("Value of coin is {}", value_in_cents(&coin));
+    println!("Value of coin is {}", coin.value_in_cents());
 
     let coin: Coin = Coin::Quarter;
-    println!("Value of coin is {}", value_in_cents(&coin));
-}
-
-fn value_in_cents(coin: &Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    }
+    println!("Value of coin is {}", coin.value_in_cents());
 }
